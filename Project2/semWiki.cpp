4ab2fd114f78313b0719d3beb44633ef89e87c3e
lbremer <lbremer@wustl.edu>
2015-11-11 19:24:53 -0600

    Fixed bugs for case of lone philosopher.

--- Project2/semWiki.cpp
+++ Project2/semWiki.cpp
@@ -15,60 +15,81 @@
 #define MAX_THINK 2
 #define MAX_EAT 2
 
-int NUM_THREADS;
-//pthread_mutex_t mut[NUM_THREADS];
-//sem_t forkArray[NUM_THREADS];
+static int MAX_ITERS = 3;
+int NUM_FORKS;
+int NUM_DINERS;
 sem_t* forkArray; // array of fork semaphore pointers
 
 void *perform_work(void *argument)
 {
-	int passed_in_value;
-	passed_in_value = *((int *) argument);
-	while(true)
-	{
-		if(DEBUG)
-			printf("%d Thinking\n", passed_in_value);
-		int thinkTime = MIN_TIME + ( rand() % (MAX_THINK - MIN_TIME));
-		if(DEBUG)
-			printf("\tThink for %d secs\n", thinkTime);
-		sleep(thinkTime);
-		//pthread_mutex_lock(&mut[passed_in_value % NUM_THREADS]);
-	        sem_wait(&forkArray[passed_in_value % NUM_THREADS]);  //Access the lower-numbered fork first.
-		//pthread_mutex_lock(&mut[(passed_in_value + 1) % NUM_THREADS]); 
-	        sem_wait(&forkArray[(passed_in_value + 1) % NUM_THREADS]);  //Then access the higher-numbered fork.
-		if(DEBUG)
-			printf("%d Eating\n", passed_in_value);
-		int eatTime = MIN_TIME + ( rand() % (MAX_EAT - MIN_TIME));
-		if(DEBUG)
-			printf("\tEat for %d secs\n", eatTime);
-		sleep(eatTime);
-		//pthread_mutex_unlock(&mut[passed_in_value % NUM_THREADS]);
-		sem_post(&forkArray[passed_in_value % NUM_THREADS]);
-		//pthread_mutex_unlock(&mut[(passed_in_value + 1) % NUM_THREADS]);
-		sem_post(&forkArray[(passed_in_value + 1) % NUM_THREADS]);
-		if(DEBUG)
-			printf("%d Finished Eating\n",passed_in_value);
-	}
-	return NULL;
+    int passed_in_value;
+    passed_in_value = *((int *) argument);
+    int count = 0;
+    while(count < MAX_ITERS)
+    {
+        if(DEBUG)
+            printf("%d Thinking\n", passed_in_value);
+        int thinkTime = MIN_TIME + ( rand() % (MAX_THINK - MIN_TIME));
+        if(DEBUG)
+            printf("\tThink for %d secs\n", thinkTime);
+        sleep(thinkTime);
+        //Access the lower-numbered fork first.
+        sem_wait(&forkArray[passed_in_value % NUM_FORKS]);  
+        //Then access the higher-numbered fork.
+        if(NUM_DINERS > 1)
+        {
+            sem_wait(&forkArray[(passed_in_value + 1) % NUM_FORKS]); 
+        }
+        else if (NUM_DINERS == 1)
+        {
+            sem_wait(&forkArray[1]); 
+        } 
+        if(DEBUG)
+            printf("%d Eating\n", passed_in_value);
+        int eatTime = MIN_TIME + ( rand() % (MAX_EAT - MIN_TIME));
+        if(DEBUG)
+            printf("\tEat for %d secs\n", eatTime);
+        sleep(eatTime);
+        sem_post(&forkArray[passed_in_value % NUM_FORKS]);
+        sem_post(&forkArray[(passed_in_value + 1) % NUM_FORKS]);
+        if(DEBUG)
+            printf("%d Finished Eating\n",passed_in_value);
+        count++;
+    }
+    return NULL;
 }
  
 int main(void)
 {
     std::cout << "Enter number of philosophers from 1 to 15" << std::endl;
-    std::cin >> NUM_THREADS;
+    std::cin >> NUM_DINERS;
     //Initialize forks as binary semaphores
-    forkArray = new sem_t[NUM_THREADS];
-    for(int idx=0; idx<NUM_THREADS; idx++)
+    if(NUM_DINERS > 1)
+    {
+        NUM_FORKS = NUM_DINERS;
+        forkArray = new sem_t[NUM_FORKS];
+    }
+    else if(NUM_DINERS == 1) //One diner is degenerate case; still need two forks
+    {
+        NUM_FORKS = NUM_DINERS + 1;
+        forkArray = new sem_t[NUM_FORKS];
+    }
+    else 
+    {
+        std::cout << "Error with NUM_DINERS" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    for(int idx=0; idx<NUM_FORKS; idx++)
     {
 
         sem_init(&forkArray[idx], 0, 1);
     }
     //Set up the threads
-    pthread_t threads[NUM_THREADS];
-    int thread_args[NUM_THREADS];
+    pthread_t threads[NUM_DINERS];
+    int thread_args[NUM_DINERS];
     int result_code, index;
     // create all threads one by one
-    for (index = 0; index < NUM_THREADS; ++index) {
+    for (index = 0; index < NUM_DINERS; ++index) {
         thread_args[index] = index;
         if(DEBUG)
             printf("In main: creating thread %d\n", index);
@@ -76,7 +97,7 @@ int main(void)
         assert(0 == result_code);
     }
     // wait for each thread to complete
-    for (index = 0; index < NUM_THREADS; ++index) {
+    for (index = 0; index < NUM_DINERS; ++index) {
         // block until thread 'index' completes
         result_code = pthread_join(threads[index], NULL);
         if(DEBUG)
